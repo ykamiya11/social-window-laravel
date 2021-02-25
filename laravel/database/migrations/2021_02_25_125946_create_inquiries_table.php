f2279e3a6d1221c57d3acd91755234b06b09cbc2
EC2 Default User <ec2-user@ip-172-31-29-141.ec2.internal>
2021-02-25 22:21:13 +0900

    テーブル作成

---
+++ laravel/database/migrations/2021_02_25_125946_create_inquiries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInquiriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inquiries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            
+            //ここから追加
+            $table->string('name');
+            $table->string('email');
+            $table->string('relationship');
+            $table->text('content');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inquiries');
+    }
+}
